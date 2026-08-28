9b6e4bcc981230b64d26623ad86dac688e52f02a
dringsim <dringsim@qq.com>
2026-08-28 18:20:13 +0800

    wallpaper

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -46,6 +46,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			"assets/images/wallpaper/d5.jpg",
 			"assets/images/wallpaper/d6.jpg",
 			"assets/images/wallpaper/d7.jpg",
+			"assets/images/wallpaper/d8.jpg",
 		],
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
@@ -57,6 +58,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			"assets/images/wallpaper/m5.jpg",
 			"assets/images/wallpaper/m6.jpg",
 			"assets/images/wallpaper/m7.jpg",
+			"assets/images/wallpaper/m8.jpg",
 		],
 		// 背景视频播放地址
 		// 支持单个视频路径（字符串）或多个视频循环（数组，参考上面壁纸配置）
